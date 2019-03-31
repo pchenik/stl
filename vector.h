bea569d5647cd989114929ececbfb43611756cc8
pchenik <49047113+pchenik@users.noreply.github.com>
2019-03-31 18:42:30 +0300

    Update vector.h
    Added logN allocation

--- vector.h
+++ vector.h
@@ -9,9 +9,9 @@ struct Less { bool operator()(const T& a, const T& b) {return a < b;} };
 template<class T>
 struct Vector {
 
-    explicit Vector(size_t size, const T &value = T()) : len(size) {
-        array = (T *) new char[len * sizeof(T)];
-        for (size_t i = 0; i < len; ++i)
+    explicit Vector(size_t size, const T &value = T()) : __size(size) {
+        array = (T *) new char[__size * sizeof(T)];
+        for (size_t i = 0; i < __size; ++i)
             new(array + i) T(value);
     }
     Vector();
@@ -25,20 +25,22 @@ struct Vector {
     void pop_back();
     void clear();
     template<class  Comp = Less<T>>
-    T minimum(const Vector& a);
+    T minimum(Comp less);
     bool empty() const;
     size_t size() const;
     T &operator[](size_t i);
     const T * const begin();
-    const T &back() const { return *(array + len - 1); }
-    const T &operator[](size_t i) const { return *(array + i); }
+    const T * const end();
+    const T &back() const { return *(array + __size - 1); }
+    //const T &operator[](size_t i) const;
 
 private:
     T *array;
-    size_t len;
-    size_t memory_size;
+    size_t __size;
+    size_t __capacity;
 };
 
 #include "vector_imp.tpp"
 
 #endif //VECTOR_VECTOR_H
+
